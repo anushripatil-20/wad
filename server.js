8ccc135b83bf5a828211834155c9ca5749e14537
anushripatil-20 <anushripatil2004@gmail.com>
2025-05-10 08:52:35 +0530

    add new

---
+++ server.js
@@ -0,0 +1,11 @@
+// server.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Serve static files from "public" directory
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
